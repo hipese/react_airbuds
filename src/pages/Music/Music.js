530b2c7dc8ba79cb7b65a7791bcbdc17fb1075d6
Khwiba <typoon141@gmail.com>
2023-12-06 16:47:05 +0900

    하단 플레이어 추가

--- src/pages/Music/Music.js
+++ src/pages/Music/Music.js
@@ -1,10 +1,17 @@
 import react from "react";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import styles from "./Music.module.css";
+import { Grid } from "@mui/material";
 
 const Music = () => {
     return (
-        <div>
-            <h1>Music</h1>
-        </div>
+        <Grid container>
+            <Grid item className={styles.user_info}>
+                <Avatar alt="Remy Sharp" sx={{ width: 200, height: 200, marginLeft: 2 }}/>
+            </Grid>
+        </Grid>
     );
 }
 
