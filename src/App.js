ca5cfb4ea3e91bab8c69b11f7389c822b79101d6
KMS <msg_1218@naver.com>
2023-12-12 16:37:43 +0900

    새로고침시 로그인 유지 구현, 로그아웃 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Groovy from "./pages/Groovy/Groovy";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { CookiesProvider } from "react-cookie";
+import axios from 'axios';
 
 
 export const LoginContext = createContext();
@@ -17,6 +18,12 @@ function App() {
     'https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/5.mp3',
     // Add more audio URLs as needed
   ]);
+
+  useEffect(()=>{
+    axios.get("/api/member/isLogined").then(resp => {
+      setLoginID(resp.data);
+    }).catch(()=>{})
+  }, [])
 
   return (
     <Router>
