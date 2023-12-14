f305da3e44dc4fbe5e82783fbe915b0d79f7967c
kojaehoon <kojae0103@naver.com>
2023-12-14 10:40:29 +0900

    playlistCreated

--- src/pages/Main/CarouselModal/CarouselModal.js
+++ src/pages/Main/CarouselModal/CarouselModal.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './CarouselModal.module.css';
 import X from '../assets/x.png';
+import axios from 'axios';
 
 const CarouselModal = ({ onClose, trackInfo }) => {
-    console.log(trackInfo);
+    const [playlistTitle, setPlaylistTitle] = useState("");
+    const [playlistVisibility, setPlaylistVisibility] = useState('public');
+    
+    const handleTitleChange = (e) => {
+        setPlaylistTitle(e.target.value);
+    };
+
+    const handleVisibilityChange = (e) => {
+        setPlaylistVisibility(e.target.value);
+    };
+
+    const handleSubmit = async () => {
+        const playlistData = {
+            playlistPlTitle: playlistTitle,
+            playlistVisibility: playlistVisibility,
+            playlistTrackId: trackInfo.track.trackId,
+            playlistTitle: trackInfo.track.title,
+            playlistImagePath: trackInfo.track.trackImages[0].imagePath,
+            playlistDuration: trackInfo.track.duration,
+            playlistFilePath: trackInfo.track.filePath,
+            playlistWriteId: trackInfo.track.writeId,
+            playlistWriter: trackInfo.track.writer,
+        }
+        try {
+            const response = await axios.post("/api/playlist", playlistData);
+            console.log(response);
+            onClose();
+        } catch (err) {
+            console.log("Error Sending Playlist Data: ", err);
+        }
+    }
+
     return (
         <div className={styles.modalBackground}>
             <div className={styles.modalContent}>
@@ -14,13 +46,13 @@ const CarouselModal = ({ onClose, trackInfo }) => {
                 </ul>
                 <div className={styles.playlistTitle}>
                     <div className={styles.plTitle}>플레이리스트 제목 <span>*</span></div>
-                    <div className={styles.plTitleInput}><input type="text" placeholder='제목 추가하기' /></div>
+                    <div className={styles.plTitleInput}><input type="text" placeholder='제목 추가하기' value={playlistTitle} onChange={handleTitleChange} /></div>
                 </div>
                 <div className={styles.playlistShare}>
                     <div className={styles.plShare}>공개 여부 : </div>
-                    <input type="radio" name="playlistVisibility"/><label htmlFor="public">전체 공개</label>
-                    <input type="radio" name="playlistVisibility"/><label htmlFor="private">비공개</label>
-                    <button className={styles.submitBtn}>저장</button>
+                    <input type="radio" id="public" value="public" name="playlistVisibility" checked={ playlistVisibility === 'public' } onChange={handleVisibilityChange}/><label htmlFor="public">전체 공개</label>
+                    <input type="radio" id="private" value="private" name="playlistVisibility" checked={ playlistVisibility === 'private' } onChange={handleVisibilityChange}/><label htmlFor="private">비공개</label>
+                    <button className={styles.submitBtn} onClick={handleSubmit}>저장</button>
                 </div>
                 <ul className={styles.addPlaylist}>
                     <li className={styles.playlist}>
