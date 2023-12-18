fb588993638be9bc48aa49e50b6afd5e2b9425e6
JawonHwang <jackie4386@naver.com>
2023-12-18 15:32:59 +0900

    ReportList&Detail Add

--- src/pages/Report/Sanction/SanctionList.js
+++ src/pages/Report/Sanction/SanctionList.js
@@ -1,10 +1,10 @@
 
 const SanctionList = () => {
-    return (
-        <div className="container">
-       
-      </div>
-    );
+  return (
+    <div className="container">
+      제재 리스트
+    </div>
+  );
 };
 
 export default SanctionList;
