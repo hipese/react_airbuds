8aca0a2b5773cfea7a75b13c47ab9eb5e6d32343
hipes <ansj2916@gmail.com>
2023-12-21 09:05:32 +0900

    GROOVY-41 앨범 수정 및 삭제

--- src/pages/Upload/MyAlbums/MyAlbumDetail/UpdateAlbumModal/UpdateAlbumModal.js
+++ src/pages/Upload/MyAlbums/MyAlbumDetail/UpdateAlbumModal/UpdateAlbumModal.js
@@ -28,18 +28,22 @@ const UpdateAlbumModal = React.forwardRef(({ albumUpdate, setAlbumUpdate, onClos
 
     // 선택된 태그를 가져오는 방법
     const [trackTags, setTrackTags] = useState([]);
-    const [backUpAlbum, setBackUpAlbum] = useState(albumUpdate);
     const [titleImage, setTitleImage] = useState();
 
     const [imageView, setImageView] = useState("/tracks/image/" + albumUpdate.coverImagePath);
     const [prevImage, setPrevImage] = useState(albumUpdate.coverImagePath);
     const [files, setFiles] = useState([]);
 
-    const [albumTag, setAlbumTag] = useState([]);
 
     const [deleteTrack, setDeleteTrack] = useState([]);
 
+    
+    // 그냥 닫을때는 다시 원래 데이터로 설정
     const handleCancle = () => {
+        onClose();
+    }
+
+    const handleUpdateComplet = () => {
         onClose();
     }
 
@@ -51,16 +55,14 @@ const UpdateAlbumModal = React.forwardRef(({ albumUpdate, setAlbumUpdate, onClos
         hiddenAudioInput.current.click();
     };
 
-    console.log(imageView);
-
     const handleUpdate = () => {
         const formData = new FormData();
         // 보내야할 데이터들 
         console.log(albumUpdate)
         console.log(trackTags)
         console.log(titleImage);
-        
-        if(files && files.length > 0){
+
+        if (files && files.length > 0) {
             files.forEach((fileData, index) => {
                 formData.append(`file`, fileData.file);
                 formData.append(`name`, fileData.name);
@@ -70,10 +72,10 @@ const UpdateAlbumModal = React.forwardRef(({ albumUpdate, setAlbumUpdate, onClos
                 // 각 파일에 대한 태그 ID 배열 추출 및 추가
             });
         }
-        
+
         // 이미지 보내기
         console.log("Title Image: ", titleImage);
-        if(titleImage){
+        if (titleImage) {
             formData.append("titleImage", titleImage);
         }
 
@@ -94,7 +96,7 @@ const UpdateAlbumModal = React.forwardRef(({ albumUpdate, setAlbumUpdate, onClos
         albumUpdate.tracks.forEach(track => {
             formData.append('Tracktitles', track.title);
         });
-      
+
 
         if (deleteTrack) {
             deleteTrack.forEach(track => {
@@ -105,16 +107,21 @@ const UpdateAlbumModal = React.forwardRef(({ albumUpdate, setAlbumUpdate, onClos
         trackTags.forEach(tag => {
             formData.append('trackTags', tag.id);
         });
-        formData.append("albumId",albumUpdate.albumId);
+        formData.append("albumId", albumUpdate.albumId);
 
         axios.post("/api/album/updateAlbum", formData, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
         }).then(resp => {
-            console.log("성공");
+            console.log("성공!");
             console.log(resp.data);
+            setAlbumUpdate(resp.data);
             setFiles([]);
+            setTitleImage();
+            setImageView();
+            setDeleteTrack();
+            handleUpdateComplet();
 
         }).catch(resp => {
             console.log("실패")
